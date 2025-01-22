13b16502f9a7a650191ba12704606660df896816
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-01-22 12:56:17 -0700

    testing links

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,29 @@ import CardList from "../components/CardList";
 export default function Home() {
   const items = [
     {
-      link: "https://example.com",
+      link: "https://www.netflix.com/browse",
       photo: "/images/example.jpg",
-      title: "Example Site",
+      title: "Netflix",
+    },
+    {
+      link: "https://www.hulu.com/hub/home",
+      photo: "/images/another-example.jpg",
+      title: "Hulu",
+    },
+    {
+      link: "https://play.max.com/profile-picker",
+      photo: "/images/another-example.jpg",
+      title: "HBO Max",
+    },
+    {
+      link: "https://tv.apple.com/channel/tvs.sbd.4000?mttn3pid=Google%20AdWords&mttnagencyid=a5e&mttncc=US&mttnsiteid=143238&mttnsubad=OUS2019801_1-688276416659-c&mttnsubkw=75222244984__idXQ476O_&mttnsubplmnt=0AAAAAC0Rhi3dsOj5qrnE_cXuyVePQiuTx",
+      photo: "/images/another-example.jpg",
+      title: "Apple",
     },
     {
       link: "https://anotherexample.com",
       photo: "/images/another-example.jpg",
-      title: "Another Example",
+      title: "Hulu",
     },
   ];
 
